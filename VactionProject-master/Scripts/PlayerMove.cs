cf9ffb97de02387200c52197312447ed1b4e214f
chan <rkdcks020328@gmail.com>
2018-07-23 22:41:08 +0900

    plz

---
+++ VactionProject-master/Scripts/PlayerMove.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+    public float MovePower = 1f;
+    public float JumpPower = 1f;
+
+    Rigidbody2D rigidbody;
+
+    Vector3 movement;
+    bool isJumping;
+
+	// Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            isJumping = true;
+        }
+	}
+
+    void FixedUpdate()
+    {
+        Move();
+        Jump();
+    }
+
+    void Move()
+    {
+        Vector3 moveVelocity = Vector3.zero;
+
+        if(Input.GetAxis("Horizontal") < 0)
+        {
+            moveVelocity = Vector3.left;
+        }
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            moveVelocity = Vector3.right;
+        }
+
+        transform.position += moveVelocity * MovePower * Time.deltaTime;
+    }
+    void Jump()
+    {
+        if (!isJumping)
+            return;
+
+        rigidbody.velocity = Vector2.zero;
+
+        Vector2 jumpVelocity = new Vector2(0, JumpPower);
+        rigidbody.AddForce(jumpVelocity, ForceMode2D.Impulse);
+
+        isJumping = false;
+    }
+}
